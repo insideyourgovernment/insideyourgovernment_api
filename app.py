d27ab1c174a92646e223ab4f93d99d9e2e6c92b7
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-19 16:45:49 +0000

    start with more complex tornado example

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class VersionHandler(tornado.web.RequestHandler):
  
 class GetGameByIdHandler(tornado.web.RequestHandler):
     def get(self, id):
-        response = { 'id': int(id),
+        response = { 'id': int(id), 
                      'name': 'Crazy Game',
                      'release_date': date.today().isoformat() }
         self.write(response)
