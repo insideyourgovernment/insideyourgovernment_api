c5615fe7d1784f02d94036987b69f9faca32fab6
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-19 07:57:54 +0000

    install script

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 # wget -q -O - https://raw.githubusercontent.com/peoplesnsallc/peoples_nsa_api/master/install.py | sudo python
 import os
-
+os.system("sudo sed -i 's;us-west-2.ec2.archive.ubuntu.com;us.archive.ubuntu.com;' /etc/apt/sources.list")
 os.system('sudo apt-get -y install git')
 os.system('source /etc/lsb-release && echo "deb http://download.rethinkdb.com/apt $DISTRIB_CODENAME main" | sudo tee /etc/apt/sources.list.d/rethinkdb.list')
 os.system('wget -qO- https://download.rethinkdb.com/apt/pubkey.gpg | sudo apt-key add -')
