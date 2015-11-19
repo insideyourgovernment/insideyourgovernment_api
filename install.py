0bc362bae6b629022f0e8677cbe6ea19467914a1
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-19 07:49:28 +0000

    install script

--- install.py
+++ install.py
@@ -1,7 +1,7 @@
 # wget -q -O - https://raw.githubusercontent.com/peoplesnsallc/peoples_nsa_api/master/install.py | sudo python
 import os
 
-os.system('sudo apt-get install git')
-os.system('sudo apt-get install rethinkdb')
+os.system('sudo apt-get -y install git')
+os.system('sudo apt-get -y install rethinkdb')
 os.system('git clone https://github.com/peoplesnsallc/peoples_nsa_api.git')
 os.system('cd peoples_nsa_api; python update.py')
