d30a15171248391ae049ff6c732f2569de019c4a
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-23 02:32:52 +0000

    auto commit

--- update.py
+++ update.py
@@ -13,7 +13,7 @@ def setup_rethinkdb():
     except:
         pass
     db = r.db("public")
-    dbs_and_tables = {'nonpublic': ['third_party_creds', 'subscribers', 'users', 'sessions'], 'public': ['crawling_instructions', 'apps', 'police_internal_affairs_cases']}
+    dbs_and_tables = {'nonpublic': ['third_party_creds', 'subscribers', 'users', 'sessions'], 'public': ['crawling_instructions', 'apps', 'police_internal_affairs_cases', 'organizations']}
     for database in dbs_and_tables.keys():
         try:
             r.db_create(database).run()
