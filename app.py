6fc05603f517968384b2a35736338395e23b89e1
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-23 07:55:46 +0000

    auto commit

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ class RetriveHandler(BaseHandler):
                 fields = list(itertools.chain.from_iterable(fields))
                 results = sorted(list(set(fields)))
             elif payload['action'] == 'count':
-                results = {'count': dbobj
+                results = {'count': dbobj.count().run()}
         else:
             results = list(dbobj.run())
         self.write(json.dumps(results))
