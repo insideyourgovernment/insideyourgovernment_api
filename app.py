e529092fa3d1da5f021a653ea6f6b8b3a9d70786
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-23 07:37:25 +0000

    auto commit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import json
 import urlparse
 import random
 import string
+import itertools
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
