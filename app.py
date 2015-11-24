9ec24fd0f7d35ba52c35c389596cecce2d6ddcd7
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-24 14:28:54 +0000

    auto commit

--- app.py
+++ app.py
@@ -210,7 +210,7 @@ class RetriveHandler(BaseHandler):
                 results = {'data': d, 'table_fields': table_fields, 'keys': d.keys()}
         else:
             results = {'data': list(dbobj.run())}
-            results['table'] = r.db('public').table('tables').get(payload['table']).
+            results['table'] = r.db('public').table('tables').get(payload['table']).run()
         self.write(json.dumps(results))
 
                         
