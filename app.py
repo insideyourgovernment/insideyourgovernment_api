3f3b294b8b927e01689e6d6d3221dc96e46d9898
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-25 16:10:58 +0000

    auto commit

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ class RetriveHandler(BaseHandler):
             results['percentages'] = []
             likely_boolean_fields = [field for field in results['fields'] if field.startswith('is_')]
             for field in likely_boolean_fields:
-                g
+                get_field(rs
             results['payload'] = payload
         self.write(json.dumps(results))
 
