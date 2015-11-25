eee52515f103a85bbd882c833996c97b28661654
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-25 15:00:29 +0000

    auto commit

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ class RetriveHandler(BaseHandler):
                 elif key == 'match_any_field':
                     dbobj = getattr(dbobj, 'filter')(lambda doc: doc.coerce_to('string').match(payload['match_any_field']))
                 elif key == 'has_string_in_any_field':
-                    dbobj = getattr(dbobj, 'filter')(lambda doc: doc.coerce_to('string').match('.*?'+payload['has_string_in_any_field']+'.*?'))
+                    dbobj = getattr(dbobj, 'filter')(lambda doc: doc.coerce_to('string').match('(?i).*?'+payload['has_string_in_any_field']+'.*?'))
                 else:
                     dbobj = getattr(dbobj, key)(payload[key])
         if 'pluck' in payload:
