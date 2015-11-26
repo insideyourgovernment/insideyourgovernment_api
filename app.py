7d303a60de9898d2ace94efb614cad2773d0400e
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-26 05:55:20 +0000

    auto commit

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ class RetriveHandler(BaseHandler):
                 percentage = "{:.0%}".format(percentage)+' (%s/%s)' % (numerator, denominator)
                 row_name = payload['table'].replace('_', ' ')
                 if payload['has_string_in_any_field']:
-                    
+                    sentence = '%s of %s mentioning "%s" are %s.' % (percentage, row_name, payload['has_string_in_any_field'], field[3:])
                 else:
                     sentence = '%s of %s are %s.' % (percentage, row_name, field[3:])
                 results['percentages'].append({'field': field, 'value': True, 'percentage': percentage, 'sentence': sentence})
