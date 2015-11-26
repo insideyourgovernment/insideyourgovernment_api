8a9bcd2e8c796e91af83bdd569380fd06d8bb2ba
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-26 05:33:39 +0000

    auto commit

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ class RetriveHandler(BaseHandler):
                 percentage = float(numerator)/denominator
                 percentage = "{:.0%}".format(percentage)+' (%s/%s)' % (numerator, denominator)
                 row_name = payload['table'].split('_')[-1]
-                sentence = '%s of %s are %s.' % (payload['table'], row_name, percentage, field)
+                sentence = '%s of %s are %s.' % (row_name, percentage, field)
                 results['percentages'].append({'field': field, 'value': True, 'percentage': percentage, 'sentence': sentence})
             results['payload'] = payload
             
