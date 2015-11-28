9a370ef4b25b62fed487645d9cf50ac71b5248ec
Tim Clemans <tclemans@peoplesnsa.com>
2015-11-28 21:30:35 +0000

    auto commit

--- crawlers/__init__.py
+++ crawlers/__init__.py
@@ -3,9 +3,14 @@ import hashlib
 import os
 from bs4 import BeautifulSoup
 import urlparse
+import re
 
 base = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../')
 
+def process_url(base_url, url):
+    print base_url, url
+    return 'http://'+urlparse.urljoin(urlparse.urlparse(base_url)['netloc'], url) if url.startswith('/') else urlparse.urljoin(base_url, url)
+ 
 def get_text_of_all_pdfs_linked_from(url):
     url_hash = hash_object = hashlib.md5(url)
     url_hash = hash_object.hexdigest()
@@ -13,13 +18,21 @@ def get_text_of_all_pdfs_linked_from(url):
     os.system('mkdir %s.crawler_data/%s/' % (base, url_hash))
     os.system('mkdir %s.crawler_data/%s/pdfs/' % (base, url_hash))
     os.system('mkdir %s.crawler_data/%s/txts/' % (base, url_hash))
-    soup = BeautifulSoup(requests.get(url).text)
+    # <base href="http://www.seattle.gov/" />	
+    html = requests.get(url).text
+    soup = BeautifulSoup(html)
+    base_url = url
+    m = re.search('<base href="(?P<url>.*?)"', html)
+    if m:
+        base_url = m.group('url')
     
-    docs = [{'url': 'http://'+urlparse.urljoin(urlparse.urlparse(url)['netloc'], link['href']) if link['href'].startswith('/') else urlparse.urljoin(url, link['href']), 'tag': link.contents[0]} for link in soup.find_all('a', href=True) if link['href'].endswith('.pdf')]
+    
+    docs = [{'url': process_url(base_url, link['href']), 'tag': link.contents[0]} for link in soup.find_all('a', href=True) if link['href'].endswith('.pdf')]
     for doc in docs:
         doc['filename'] = doc['url'].split('/')[-1]
     for doc in docs:
-        if not doc['filename'] in os.listdir(base+'.crawler_data/'+url_hash+'/pdfs'):
+        if True:
+        #if not doc['filename'] in os.listdir(base+'.crawler_data/'+url_hash+'/pdfs'):
             os.system('wget -O %s.crawler_data/%s/pdfs/%s %s' % (base, url_hash, doc['filename'], doc['url']))
      
             os.system('pdf2txt.py -A %s.crawler_data/%s/pdfs/%s > %s.crawler_data/%s/txts/%s' % (base, url_hash, doc['filename'], base, url_hash, doc['filename'][:-4]+'.txt'))
