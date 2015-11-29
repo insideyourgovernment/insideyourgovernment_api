b4b1a33764dfcf50c4041489bc3557ac6c5be9f3
Tim Clemans <tim@insideyourgovernment.com>
2015-11-29 21:39:32 +0000

    auto commit

--- app.py
+++ app.py
@@ -175,7 +175,8 @@ class RetriveHandler(BaseHandler):
             d = {"left": r.row["left"], "right": {}}
             for right_field in right_fields:
                 d[field[:-2]+right_field] = r.row["right"][right_field]
-            
+            dbobj = getattr(dbobj, 'map')(d)
+            dbobj = 
             
         #for data in joined_data:
         #    d = {}
