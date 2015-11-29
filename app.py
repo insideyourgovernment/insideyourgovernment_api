31742c4de61e37859d4f4e02dc126923fa94d20b
Tim Clemans <tim@insideyourgovernment.com>
2015-11-29 21:35:58 +0000

    auto commit

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ class RetriveHandler(BaseHandler):
             fields = sorted(list(set(fields)))
             dbobj = getattr(dbobj, 'eq_join')(field, r.db("public").table(field[:-3]))
             d = {"left": r.row["left"], "right": {}}
-            for right_field i
+            for right_field in 
         #for data in joined_data:
         #    d = {}
         #    d.update(data['left'])
