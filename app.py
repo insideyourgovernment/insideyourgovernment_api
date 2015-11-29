7ea78c9ece4be974131764fc78b7e1d255a7cd5b
Tim Clemans <tim@insideyourgovernment.com>
2015-11-29 21:38:08 +0000

    auto commit

--- app.py
+++ app.py
@@ -175,6 +175,7 @@ class RetriveHandler(BaseHandler):
             d = {"left": r.row["left"], "right": {}}
             for right_field in right_fields:
                 d[field[:-2]+right_field] = r.row["right"][right_field]
+            
         #for data in joined_data:
         #    d = {}
         #    d.update(data['left'])
