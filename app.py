81c76b38c43e909bd22ef36487688e2af19be82a
Tim Clemans <tim@insideyourgovernment.com>
2015-11-29 21:53:27 +0000

    auto commit

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ class RetriveHandler(BaseHandler):
             dbobj = getattr(dbobj, 'eq_join')(field, r.db("public").table(field[:-3]+'s'))
             d = {"left": r.row["left"], "right": {}}
             for right_field in right_fields:
-                d["right"field[:-2]+right_field] = r.row["right"][right_field]
+                d["right"][field[:-2]+right_field] = r.row["right"][right_field]
             dbobj = dbobj.map(d)
             dbobj = dbobj.zip()
             
