51faf3fd0d6949fdb5d258c813baa10390a12d63
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 07:50:20 +0000

    auto commit

--- app.py
+++ app.py
@@ -291,8 +291,9 @@ class RetriveHandler(BaseHandler):
             for field in results['fields']:
                 if not field.endswith('_id'):
                     continue
-                g = r.db('public').table(
-                results['group_counts'][field] = [[item[0], item[1]] for item in list(sorted(dbobj.group(field).count().run().items(), key=lambda x:x[1], reverse=True))]
+                t = special_names[field[:-3]] if field[:-3] in special_names else field[:-3]+'s'
+                g = r.db('public').table(t).group('id').run()
+                results['group_counts'][field] = [[g[item[0]], item[1]] for item in list(sorted(dbobj.group(field).count().run().items(), key=lambda x:x[1], reverse=True))]
                 
             likely_boolean_fields = [field for field in results['fields'] if field.startswith('is_')]
             # remove if the field in a filter
