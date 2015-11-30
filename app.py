4e0e9fa89bc964cef9d9cfe7e017b4d11c1ba413
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 07:39:06 +0000

    auto commit

--- app.py
+++ app.py
@@ -290,7 +290,7 @@ class RetriveHandler(BaseHandler):
             results['group_counts'] = {}
             for field in results['fields']:
                 try:
-                    results['group_counts'][field] = list(sorted(dbobj.group(field).count().run().items(), key=lambda x:x[1], reverse=True))
+                    results['group_counts'][field] = [item[0], item[1]]list(sorted(dbobj.group(field).count().run().items(), key=lambda x:x[1], reverse=True))
                 except:
                     pass
             likely_boolean_fields = [field for field in results['fields'] if field.startswith('is_')]
