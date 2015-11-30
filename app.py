6f2408032d12c3e71c44d29777c5970ac2d6a062
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 02:07:39 +0000

    auto commit

--- app.py
+++ app.py
@@ -179,9 +179,9 @@ class RetriveHandler(BaseHandler):
             #for right_field in right_fields:
             #    d["right"][field[:-2]+right_field] = r.row["right"][right_field]
             #dbobj = dbobj.map(d)
-            dbobj = dbobj.merge(  lambda row: {'right': row['right'].coerce_to('array').map(
+            dbobj = dbobj.merge(  lambda row: {'right': row['right'].coerce_to('object').map(
                           lambda pair: [r.expr(field[:-2]) + pair[0], pair[1]]
-                        ).coerce_to('array')})
+                        ).coerce_to('object')})
             #            )})
             dbobj = dbobj.zip()
             
