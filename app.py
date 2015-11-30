40b23b8a8c20964c2c7217555db236de8f12b1a3
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 03:38:56 +0000

    auto commit

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ class RetriveHandler(BaseHandler):
             #dbobj = dbobj.map(d)
             dbobj = dbobj.merge(  lambda row: {'left': row['right'].coerce_to('array').map(
                           lambda pair: [r.expr(field[:-2]) + pair[0], pair[1]]
-                        ).coerce_to('object')}).with({'right': True}).zip()
+                        ).coerce_to('object')}).with_out({'right': True}).zip()
             #dbobj = dbobj
             
             
