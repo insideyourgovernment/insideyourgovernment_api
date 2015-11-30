c32ab55b99d04a67e115cea82145cb394b87f310
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 06:08:37 +0000

    auto commit

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ class RetriveHandler(BaseHandler):
             if 'default_order_by' in results['table']:
                 dbobj = getattr(dbobj, 'order_by')(r.desc(results['table']['default_order_by']['field']))
             special_names = {value: key for key, value in special_names.items()}
-            t = 
+            t = p
             t = special_names[
             results['data'] = list(dbobj.run(time_format="raw"))
             if 'linked_tables' in results['table']:
