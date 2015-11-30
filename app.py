2a4002f56b923c5d07996ebc79c42788f17440d0
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 02:18:49 +0000

    auto commit

--- app.py
+++ app.py
@@ -175,15 +175,15 @@ class RetriveHandler(BaseHandler):
             special_names = {'person': 'people'}
             t = special_names[field[:-3]] if field[:-3] in special_names else field[:-3]+'s'
             print field, t
-            dbobj = dbobj.eq_join(field, r.db("public").table(t)).zip().coerce_to('array')
+            dbobj = dbobj.eq_join(field, r.db("public").table(t))
             #d = {"left": r.row["left"], "right": {}}
             #for right_field in right_fields:
             #    d["right"][field[:-2]+right_field] = r.row["right"][right_field]
             #dbobj = dbobj.map(d)
-            #dbobj = dbobj.merge(  lambda row: {'right': row['right'].coerce_to('array').map(
-            #              lambda pair: [r.expr(field[:-2]) + pair[0], pair[1]]
-            #            ).coerce_to('array')})
-            #            )})
+            dbobj = dbobj.merge(  lambda row: {'right': row['right'].coerce_to('array').map(
+                          lambda pair: [r.expr(field[:-2]) + pair[0], pair[1]]
+                        ).coerce_to('object')})
+                        )}).zip()
             #dbobj = dbobj
             
             
