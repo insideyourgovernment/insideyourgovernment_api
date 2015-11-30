42aeb2408475bd5db69e0583c7384d2861215d64
Tim Clemans <tim@insideyourgovernment.com>
2015-11-30 06:12:43 +0000

    auto commit

--- app.py
+++ app.py
@@ -271,6 +271,7 @@ class RetriveHandler(BaseHandler):
             special_names = {value: key for key, value in special_names.items()}
             t = payload['table']
             t = special_names[t] if t in special_names else t[:-1]
+            t = t
             results['data'] = list(dbobj.run(time_format="raw"))
             if 'linked_tables' in results['table']:
                 for linked_table in results['table']['linked_tables']:
