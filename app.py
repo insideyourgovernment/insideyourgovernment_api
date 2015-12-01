1861ce17fd6a87d83df65c9c68f01099b421a7a5
Tim Clemans <tim@insideyourgovernment.com>
2015-12-01 22:15:35 +0000

    auto commit

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ class RetriveHandler(BaseHandler):
         
         if 'filter' in payload:
             key = 'filter'
-            dbobj = getattr(dbobj, key)(**payload[key])
+            dbobj = getattr(dbobj, key)(payload[key])
         print dbobj    
         #for data in joined_data:
         #    d = {}
