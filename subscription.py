ce0af600ca3a427ad58be4a98a1d4f4301d481e5
Tim Clemans <tim@insideyourgovernment.com>
2015-12-03 21:36:34 +0000

    auto commit

--- subscription.py
+++ subscription.py
@@ -11,18 +11,5 @@ def print_changes():
         change = yield feed.next()
         print(change)
 
-import rethinkdb as r
-from tornado import ioloop, gen
 
-r.set_loop_type("tornado")
-
-@gen.coroutine
-def print_changes():
-    conn = yield r.connect(host="localhost", port=28015)
-    feed = yield r.table("table").changes().run(conn)
-    while (yield feed.fetch_next())
-        change = yield feed.next()
-        print(change)
-
-ioloop.IOLoop.current().add_callback(print_changes)
 ioloop.IOLoop.current().add_callback(print_changes)
