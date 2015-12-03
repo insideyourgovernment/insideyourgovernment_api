999eb70a4ff87cbcc561b7a124ca6d8730c23422
Tim Clemans <tim@insideyourgovernment.com>
2015-12-03 22:03:07 +0000

    auto commit

--- subscription.py
+++ subscription.py
@@ -18,9 +18,5 @@ def main():
         ioloop.IOLoop.current().add_callback(print_changes, table)
         
 if __name__ == "__main__":
-    conn = r.connect(host="localhost", port=28015)
-    for table in r.db('public').table_list().run(conn):
-        ioloop.IOLoop.current().add_callback(print_changes, table)
-        #IOLoop.current().start()
     IOLoop.current().run_sync(main)
     IOLoop.current().start()
