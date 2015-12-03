85446e4fae06b38acc2c15ce84f053ddc26fbada
Tim Clemans <tim@insideyourgovernment.com>
2015-12-03 22:58:47 +0000

    auto commit

--- subscription.py
+++ subscription.py
@@ -1,6 +1,7 @@
 import rethinkdb as r
 from tornado import ioloop, gen
 from search import handle_query
+
 r.set_loop_type("tornado")
 
 @gen.coroutine
@@ -13,7 +14,7 @@ def print_changes(table):
 
 @gen.coroutine
 def main():
-    
+    handle_query
         
 if __name__ == "__main__":
     ioloop.IOLoop.current().run_sync(main)
