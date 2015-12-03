5a954d662bdd12141de2ece347c8c930350c6562
Tim Clemans <tim@insideyourgovernment.com>
2015-12-03 21:53:15 +0000

    auto commit

--- subscription.py
+++ subscription.py
@@ -12,19 +12,6 @@ def print_changes(table):
         print(change)
 
 
-@gen.coroutine
-def main():
-    """ Async main method. It needed to be async due to r.connect is async . """
-    parse_command_line()
-    db_name = "rechat"
-    setup_db(db_name)
-    r.set_loop_type("tornado")
-
-    db = yield r.connect("localhost", db=db_name)
-    #Single db connection for everything thanks a lot Ben and Jeese
-    http_server = httpserver.HTTPServer(RechatApp(db))
-    http_server.listen(options.port)
-
 if __name__ == "__main__":
     conn = r.connect(host="localhost", port=28015)
     for table in r.db('public').table_list().run(conn):
