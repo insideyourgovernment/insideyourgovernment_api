67d7a904e5d494705f8d84e6085ac652ccf4953e
Tim Clemans <tim@insideyourgovernment.com>
2015-12-03 21:51:41 +0000

    auto commit

--- subscription.py
+++ subscription.py
@@ -5,8 +5,8 @@ r.set_loop_type("tornado")
 
 @gen.coroutine
 def print_changes(table):
-    conn = yeild r.connect(host="localhost", port=28015)
-    feed = yeild r.db('public').table(table).changes().run(conn)
+    conn = yield r.connect(host="localhost", port=28015)
+    feed = yield r.db('public').table(table).changes().run(conn)
     while (yield feed.fetch_next()):
         change = yield feed.next()
         print(change)
