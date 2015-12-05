b9afd278f50a5e71cb65207032f38c396704f8e0
Tim Clemans <tim@insideyourgovernment.com>
2015-12-05 17:13:57 +0000

    auto commit

--- update_row_counts.py
+++ update_row_counts.py
@@ -16,7 +16,7 @@ def print_changes(q):
 def main():
     conn = yield r.connect(host="localhost", port=28015)
     table_list = r.db('public').table_list()
-    table_list = yield table_list.run
+    table_list = yield table_list.run(conn)
     q = handle_query({'table': 'test_table'}, run=False).changes()
     ioloop.IOLoop.current().add_callback(print_changes, q)
     
