5fac11a2b196114560915b21b91c0098a98e7b2a
Tim Clemans <tim@insideyourgovernment.com>
2015-12-05 17:10:45 +0000

    auto commit

---
+++ update_row_counts.py
@@ -0,0 +1,23 @@
+import rethinkdb as r
+from tornado import ioloop, gen
+from search import handle_query
+
+r.set_loop_type("tornado")
+
+@gen.coroutine
+def print_changes(q):
+    conn = yield r.connect(host="localhost", port=28015)
+    feed = yield q.run(conn)
+    while (yield feed.fetch_next()):
+        change = yield feed.next()
+        print(change)
+
+@gen.coroutine
+def main():
+    q = handle_query({'table': 'test_table'}, run=False).changes()
+    ioloop.IOLoop.current().add_callback(print_changes, q)
+    
+        
+if __name__ == "__main__":
+    ioloop.IOLoop.current().run_sync(main)
+    ioloop.IOLoop.current().start()
