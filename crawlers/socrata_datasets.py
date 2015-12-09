fdb88c4cfb9954b9107d179be0cad14125c9566c
Tim Clemans <tim@insideyourgovernment.com>
2015-12-09 12:28:01 +0000

    auto commit

--- crawlers/socrata_datasets.py
+++ crawlers/socrata_datasets.py
@@ -4,6 +4,7 @@ import requests
 t = list(r.db('public').table('police_response_events').order_by(r.desc('socrata_created_at'), index=r.desc('socrata_created_at')).limit(int(1)).run())[0]['socrata_created_at'].isoformat()[:-9]
 data = requests.get('https://data.seattle.gov/resource/pu5n-trf4.json?$select=:*,*&$limit=2000000&$where=:created_at%%20>%%20"%s"' % (t)).json()
 import dateutil
+import dateutil.parser
 from pytz import timezone
 from datetime import date
 from datetime import datetime
