04ff66724bc7a7168e99be646d1ec0ca6e3aa4ef
Tim Clemans <tim@insideyourgovernment.com>
2015-12-12 07:00:39 +0000

    auto commit

--- crawlers/run_crawlers.py
+++ crawlers/run_crawlers.py
@@ -1,7 +1,7 @@
 import os
 dirname, filename = os.path.split(os.path.abspath(__file__))
 for f in os.listdir(dirname):
-    dirname, filename = os.path.split(f)
+    #dirname, filename = os.path.split(f)
     if f.endswith('.py'):
-        if filename not in ['__init__.py', 'run_crawlers.py', 'seattle_police_internal_affairs_closed_case_summaries.py']:
+        if f not in ['__init__.py', 'run_crawlers.py', 'seattle_police_internal_affairs_closed_case_summaries.py']:
             os.system('python %s &' % (f))
