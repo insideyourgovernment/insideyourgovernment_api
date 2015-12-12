012ccbb723e8425da70fb2c940ab453c369d44ce
Tim Clemans <tim@insideyourgovernment.com>
2015-12-12 02:47:28 +0000

    auto commit

--- search.py
+++ search.py
@@ -195,7 +195,8 @@ def handle_query(payload, run=True):
         results = {}
         results['table'] = r.db('public').table('tables').get(payload['table']).run()
         if 'default_order_by' in results['table']:
-            if 
+            field = results['table']['default_order_by']['field']
+            if field in r.db
             dbobj = getattr(dbobj, 'order_by')(r.desc(results['table']['default_order_by']['field']))
         special_names_reversed = {value: key for key, value in special_names.items()}
         t = payload['table']
