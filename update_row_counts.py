05c2d618fc316b3a2bbaea6b70a5c96b26bc8f2f
Tim Clemans <tim@insideyourgovernment.com>
2015-12-15 10:34:58 +0000

    auto commit

--- update_row_counts.py
+++ update_row_counts.py
@@ -6,7 +6,8 @@ from utils import get_dt
 r.set_loop_type("tornado")
 def changed_keys(change):
     ck = []
-    for key in change.get()
+    for key in change.get('new_val', default={}):
+        if not key in change.get('new_val', default=[])
 @gen.coroutine
 def update_row_counts(table):
     conn = yield r.connect(host="localhost", port=28015)
