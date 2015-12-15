5c2baa3c47bbc7878b3b69f5193e18614a1ce063
Tim Clemans <tim@insideyourgovernment.com>
2015-12-15 10:24:16 +0000

    auto commit

--- utils.py
+++ utils.py
@@ -3,3 +3,9 @@ import rethinkdb as r
 
 def get_dt():
     return r.expr(datetime.now(r.make_timezone('-07:00')))
+
+def update_all_row_counts():
+    tables = list(r.db('public').table('tables').run())
+    for table in tables:
+        the_count = r.db('public').table(table['id']).count().run()
+        print table['id'], r.db('public').table('tables').get(table['id']).update({'number_of_rows': the_count}).run()
