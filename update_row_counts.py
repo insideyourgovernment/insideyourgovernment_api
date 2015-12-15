f033efacae1a7d4d19b51d9d213f4e67c152926c
Tim Clemans <tim@insideyourgovernment.com>
2015-12-15 09:59:02 +0000

    auto commit

--- update_row_counts.py
+++ update_row_counts.py
@@ -11,7 +11,7 @@ def update_row_counts(table):
     feed = yield first_part.run(conn)
     while (yield feed.fetch_next()):
         change = yield feed.next()
-        c = {'table': table, 'datetime': dt, 'change':
+        c = {'table': table, 'datetime': dt, 'change': chang
         print change
         if change.get('new_val'):
             indexes = yield r.db('public').table(table).index_list().run(conn)
