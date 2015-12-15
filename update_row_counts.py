3ba8964e1b754d9033794a6fd55c2898f80f976f
Tim Clemans <tim@insideyourgovernment.com>
2015-12-15 10:45:27 +0000

    auto commit

--- update_row_counts.py
+++ update_row_counts.py
@@ -38,20 +38,20 @@ def update_row_counts(table):
             fields = table_data.get('fields')
             for key in change['new_val']:
                 if not key in indexes and not key == 'id':
-                    print r.db('public').table(table).index_create(key).run(conn)
+                    #print r.db('public').table(table).index_create(key).run(conn)
             if not fields:
                 r.db('public').table('tables').get(table).update({'fields': change['new_val'].keys()}).run(conn)
             else:
                 for key in change['new_val']:
                     if not key in fields:
                         fields.append(key)
-                        print r.db('public').table(table).get(table).update({'fields': fields}).run(conn)
+                        #print r.db('public').table(table).get(table).update({'fields': fields}).run(conn)
         if not change['new_val'] or not change['old_val']:
             number_of_rows = r.db('public').table(table).count()
             number_of_rows = yield number_of_rows.run(conn)
             query = r.db('public').table('tables').get(table).update({'number_of_rows': number_of_rows})
             q = yield query.run(conn)
-            print table, query, q
+            #print table, query, q
 
 @gen.coroutine
 def main():
