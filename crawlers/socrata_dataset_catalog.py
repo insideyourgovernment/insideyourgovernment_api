5177d37a7a30d0bd10234e48617da1296587ade0
Tim Clemans <tim@insideyourgovernment.com>
2015-12-17 07:58:10 +0000

    auto commit

--- crawlers/socrata_dataset_catalog.py
+++ crawlers/socrata_dataset_catalog.py
@@ -1,15 +1,11 @@
 from joblib import Parallel, delayed  
 import multiprocessing
 
-# what are your inputs, and what operation do you want to 
-# perform on each input. For example...
-inputs = range(10)  
-def processInput(i):  
-    return i * i
+
 
 num_cores = multiprocessing.cpu_count()
 
-results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)  
+  
 
 import rethinkdb as r
 
@@ -53,6 +49,6 @@ def do():
         
         modified_data.append(d)
     print r.db('public').table('datasets').insert(modified_data).run(conflict='update')
-    
+    results = Parallel(n_jobs=num_cores)(delayed(processInput)(i) for i in inputs)
 while True:
     do()
