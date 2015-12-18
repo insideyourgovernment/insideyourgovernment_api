b5fbec36e27fb94d51d967ca073a25f3681cca96
Tim Clemans <tim@insideyourgovernment.com>
2015-12-18 02:33:41 +0000

    auto commit

--- crawlers/socrata_dataset_catalog.py
+++ crawlers/socrata_dataset_catalog.py
@@ -35,7 +35,7 @@ def run_count(i, theid, api_url, app_token, tables_list, d):
     
     try:
         per_page = 1000
-        
+        api_url
         for i in range(number_of_rows/per_page):
             
             data_url = '%s?$select=:*,*&$limit=%s&$offset=%s&$$app_token=%s' % (api_url, per_page, per_page * i, app_token)
