61de4a7febaf5ec6000119cb45da38f20672521e
Tim Clemans <tim@insideyourgovernment.com>
2015-12-18 01:39:33 +0000

    auto commit

--- crawlers/socrata_dataset_catalog.py
+++ crawlers/socrata_dataset_catalog.py
@@ -26,6 +26,7 @@ def run_count(i, theid, api_url, app_token, tables_list, d):
         print traceback.print_exc()
         return None
     data_url = '%s?$select=count(*)&$$app_token=%s' % (api_url, app_token)
+    per_page = 1000
     try:
         count_data = requests.get(count_url, verify=False).json()
         number_of_rows = count_data[0]['count']
