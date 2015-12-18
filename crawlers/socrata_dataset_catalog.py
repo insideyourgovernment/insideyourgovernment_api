92a85cc575da412d00d1a473970f0665e21592f4
Tim Clemans <tim@insideyourgovernment.com>
2015-12-18 02:33:33 +0000

    auto commit

--- crawlers/socrata_dataset_catalog.py
+++ crawlers/socrata_dataset_catalog.py
@@ -36,6 +36,7 @@ def run_count(i, theid, api_url, app_token, tables_list, d):
     try:
         per_page = 1000
         for i in range(number_of_rows/per_page):
+            
             data_url = '%s?$select=:*,*&$limit=%s&$offset=%s&$$app_token=%s' % (api_url, per_page, per_page * i, app_token)
             data = requests.get(data_url, verify=False).json()
             modified_data = []
