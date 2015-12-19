439ea6273f97fce76ec3a7425ea0404059876822
Tim Clemans <tim@insideyourgovernment.com>
2015-12-19 18:12:37 +0000

    auto commit

--- search.py
+++ search.py
@@ -14,7 +14,7 @@ import requests
 import os
 
 def is_number_field(field):
-    indicators = ['number', 'num', 'views', 
+    indicators = ['number', 'num', 'views', 'total']
 def get_field(l, field):
     results = []
     for d in l:
