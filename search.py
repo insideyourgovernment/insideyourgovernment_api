1a8fd163ddc13e7877b22f361481a76c06601f17
Tim Clemans <tim@insideyourgovernment.com>
2015-12-20 19:08:36 +0000

    auto commit

--- search.py
+++ search.py
@@ -31,7 +31,7 @@ def handle_query(payload, run=True):
     if run:
         r.db('public').table('queries').insert({'datetime': r.expr(datetime.now(r.make_timezone('-07:00'))), 'payload': payload}).run()
     if 'global_search_query' in payload:
-        for row in r.db('public').table('rules_for_global_search').pluck(order_by('order').run():
+        for row in r.db('public').table('rules_for_global_search').pluck('id').order_by('order').run():
             
     
     dbobj = r.db('public').table(payload['table'])
