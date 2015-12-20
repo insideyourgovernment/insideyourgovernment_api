169786e3cf686f762b6d7ad56e94c56f769bf915
Tim Clemans <tim@insideyourgovernment.com>
2015-12-20 21:53:16 +0000

    auto commit

--- search.py
+++ search.py
@@ -64,7 +64,7 @@ def handle_query(payload, run=True):
                     results['data'] = results['data'][0]
                 for sentence_rule in rules['sentences']:
                     if test_rule(sentence_rule['not_none'], results['data']):
-                        results['sentence'] = sentence_rule['se
+                        results['sentence'] = sentence_rule['sentence']
             return results
         else:
             return None
