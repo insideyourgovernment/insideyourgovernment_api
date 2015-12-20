fd237d4aec6b9b14f324c68d4184f3f51aac7941
Tim Clemans <tim@insideyourgovernment.com>
2015-12-20 21:50:56 +0000

    auto commit

--- search.py
+++ search.py
@@ -60,7 +60,8 @@ def handle_query(payload, run=True):
             rules = r.db('public').table('rules_for_global_search').get(row['id']).run()
             results = run_query(m.groupdict(), rules['query'])
             if 'sentences' in rules:
-                if 
+                if len(results['data']) == 1:
+                    results['data'] = results['data'][0]
                 for sentence_rule in rules['sentences']:
                     if 
             return results
