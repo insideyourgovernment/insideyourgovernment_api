7dcfe2551c1e4c77646f7ae0e830b296e36ab800
Tim Clemans <tim@insideyourgovernment.com>
2015-12-25 02:13:19 +0000

    auto commit

--- ws_app.py
+++ ws_app.py
@@ -25,8 +25,12 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     def on_message(self, message):
         conn = r.connect( "localhost", 28015).repl()
         print "New message {}".format(message)
+        print 'a'
         response = r.db('public').table(message).limit(10).run(conn, time_format="raw")
+        print 'b'
         response = list(response)
+        print 'c'
+        
         response = {'table': message, 'rows': response}
         self.write_message(response)
 
