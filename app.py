405c8b305a48479761e397a6f47b40a01683aae1
Tim Clemans <tim@insideyourgovernment.com>
2015-12-29 07:06:50 +0000

    auto commit

--- app.py
+++ app.py
@@ -204,12 +204,14 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         print message
         message = json.loads(message)
         if message['ws_for'] != 'count':
-            ioloop.IOLoop.current().add_callback(run_query, r.db('public').table(message['table']).get(message['get']), message['ws_for'], self)
+            
             if action in message:
                 all_functions = dict(inspect.getmembers(search, inspect.isfunction))
                 f = 'action_'+message['action']
                 if f in all_functions:
                     all_functions[f]()
+            else:
+                ioloop.IOLoop.current().add_callback(run_query, r.db('public').table(message['table']).get(message['get']), message['ws_for'], self)
         elif message['ws_for'] == 'count':
             print '***', message
             ioloop.IOLoop.current().add_callback(run_query, r.db('public').table(message['table']).count(), message['ws_for'], self)
