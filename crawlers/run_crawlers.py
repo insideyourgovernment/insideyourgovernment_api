331c58ddade19bdce81ac1e44c9372fd9ff2cbc6
Tim Clemans <tim@insideyourgovernment.com>
2016-01-05 06:28:20 +0000

    auto commit

--- crawlers/run_crawlers.py
+++ crawlers/run_crawlers.py
@@ -3,5 +3,5 @@ dirname, filename = os.path.split(os.path.abspath(__file__))
 for f in os.listdir(dirname):
     #dirname, filename = os.path.split(f)
     if f.endswith('.py'):
-        if f not in ['__init__.py', 'run_crawlers.py']:
+        if f not in ['__init__.py', 'run_crawlers.py', 'socrata_dataset_catalog.py']:
             os.system('python %s &' % (os.path.join(dirname, f)))
